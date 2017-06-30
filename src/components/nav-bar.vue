e286720e943d89509db78675ff8e9636807c6a9d
Tyler Brown <tbrown9619@gmail.com>
2017-06-30 10:25:55 -0500

    changed to using element ui built in router, saves some code, thanks docs.

--- src/components/nav-bar.vue
+++ src/components/nav-bar.vue
@@ -1,12 +1,12 @@
 <template>
-    <el-menu theme="dark" :default-active="activeIndex" mode="horizontal" @select="handleSelect">
+    <el-menu theme="dark" :default-active="activeIndex" mode="horizontal" @select="handleSelect" router=true>
     
         <el-menu-item index="/">
-            <router-link to="/" exact> Home </router-link>
+            Home
         </el-menu-item>
 
         <el-menu-item index="about">
-            <router-link to="/about" exact> About </router-link>
+            About
         </el-menu-item>
             <el-menu-item index="3"><a href="https://www.ele.me" target="_blank">Orders</a></el-menu-item>
 
@@ -22,7 +22,7 @@ export default {
     methods: {
         handleSelect(key, keyPath) {
             console.log(key, keyPath);
-            this.$router.push(key);
+            //this.$router.push(key);
         }
     }
 }
