0da34f76489b2c4b3712e99916e697ccfb88ed65
Tyler Brown <tbrown9619@gmail.com>
2017-07-12 17:10:38 -0500

    got all the dependencies and stuff working for now. installed sass loader. some other stuff. also starting work on getting the nav bar back working

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,39 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
-  </div>
+    <div id="app">
+        <navbar></navbar>
+        <router-view></router-view>
+    </div>
 </template>
 
 <script>
+import Navbar from './components/layout/NavBar.vue';
+
 export default {
-  name: 'app'
-}
+    name: 'app',
+
+    components: {
+        Navbar
+    }
+};
+
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Lato:300,400,700');
+@import url('https://fonts.googleapis.com/css?family=Righteous');
+@import url('https://fonts.googleapis.com/css?family=Roboto');
+
+body {
+    margin: 0 !important;
+    padding-bottom: 5rem;
+    font-family: 'Lato', sans-serif;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,27 +1,63 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import VeeValidate from 'vee-validate';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-import {table, TableColumn, Form, FormItem, menu, MenuItem, Tooltip, row, col} from 'element-ui'
-Vue.use(table)
-Vue.use(TableColumn)
-Vue.use(Form)
-Vue.use(FormItem)
-Vue.use(menu)
-Vue.use(MenuItem)
-Vue.use(Tooltip)
-Vue.use(row)
-Vue.use(col)
-import 'element-ui/lib/theme-default/index.css'
+import {
+    table,
+    TableColumn,
+    Form,
+    FormItem,
+    menu,
+    MenuItem,
+    Tooltip,
+    row,
+    col
+} from 'element-ui';
+Vue.use(table);
+Vue.use(TableColumn);
+Vue.use(Form);
+Vue.use(FormItem);
+Vue.use(menu);
+Vue.use(MenuItem);
+Vue.use(Tooltip);
+Vue.use(row);
+Vue.use(col);
+import 'element-ui/lib/theme-default/index.css';
+
+// Vee validate configuration must be changed so it can properly interact with element ui
+const config = {
+    errorBagName: 'errors', // change if property conflicts.
+    fieldsBagName: 'inputs ', // Default is fields
+    delay: 0,
+    locale: 'en',
+    dictionary: null,
+    strict: true,
+    enableAutoClasses: false,
+    classNames: {
+        touched: 'touched', // the control has been blurred
+        untouched: 'untouched', // the control hasn't been blurred
+        valid: 'valid', // model is valid
+        invalid: 'invalid', // model is invalid
+        pristine: 'pristine', // control has not been interacted with
+        dirty: 'dirty' // control has been interacted with
+    },
+    events: 'input|blur',
+    inject: true
+};
+
+Vue.use(VeeValidate, config);
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
-})
+    el: '#app',
+    router,
+    template: '<App/>',
+    components: {
+        App
+    }
+});
