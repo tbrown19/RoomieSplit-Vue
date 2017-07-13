087f764b3487a83ba3454fb069f7155ed9987113
Tyler Brown <tbrown9619@gmail.com>
2017-07-13 09:08:46 -0500

    added default data tests to the home page

--- test/unit/specs/Home.spec.js
+++ test/unit/specs/Home.spec.js
@@ -2,13 +2,34 @@ import Vue from 'vue';
 import Home from '@/views/Home';
 
 describe('Home.vue', () => {
-    it('should render correct contents', () => {
-        const Constructor = Vue.extend(Home);
-        const vm = new Constructor().$mount();
-        Vue.nextTick(() => {
+    const Constructor = Vue.extend(Home);
+    const vm = new Constructor().$mount();
+
+    it('sets the correct default data', () => {
+        expect(Home.data).to.be.a('function');
+        const defaultData = Home.data();
+        expect(defaultData.mainInputsCompleted).to.be.false;
+    });
+
+    describe('Header Text', () => {
+        it('should render correct contents', () => {
             expect(vm.$el.querySelector('h1').textContent)
-            .to.include('Calculate how much each person should contribute to the rent.');
-            // Since we're doing this asynchronously, we need to call done() to tell Mocha that we've finished the test.
+                .to.include('Calculate how much each person should contribute to the rent.',
+                    'Get started by filling out the information below.'
+                );
+        });
+    });
+
+    describe('Primary Inputs', () => {
+        it('should render correct contents', () => {
+            expect(vm.$el.querySelector('.card div').textContent)
+                .to.include('Rooms', 'Area', 'Rent');
+            expect(vm.$el.querySelector('.card div').textContent)
+                .to.include('Rooms', 'Area', 'Rent');
+            // expect(vm.$el.querySelector('.card'))
+            //     .to.include('Calculate how much each person should contribute to the rent.',
+            //         'Get started by filling out the information below.'
+            //     );
         });
     });
 });
