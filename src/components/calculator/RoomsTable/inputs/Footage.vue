045aef09a33996b575699c1b6b14c19df3ea92de
Tyler Brown <tbrown9619@gmail.com>
2017-09-04 11:39:11 -0500

    added a watcher that watches for change in the current area, and if it gets one, updates the editing area. before this change the editing area was keeping the same value as before, even if the user updated the new footage using the measurements

--- src/components/calculator/RoomsTable/inputs/Footage.vue
+++ src/components/calculator/RoomsTable/inputs/Footage.vue
@@ -1,15 +1,14 @@
 <template>
     <div>
-    
+
         <div v-if="editing">
-            <input class="area-input" @blur="editing = false" @input="checkArea(editingArea)" v-model.number="editingArea" v-validate="'required'" 
-            :class="{'input': true, 'is-danger': errors.has('area'), 'is-success': !errors.has('area') && this.editingArea != '' && this.editingArea != 0}" type="number" placeholder="0" name="area" value=4>
+            <input class="area-input" @blur="editing = false" @input="checkArea(editingArea)" v-model.number="editingArea" v-validate="'required'" :class="{'input': true, 'is-danger': errors.has('area'), 'is-success': !errors.has('area') && this.editingArea != '' && this.editingArea != 0}" type="number" placeholder="0" name="area" value=4>
         </div>
-    
+
         <div v-else>
             <input @click="editing = true" class="area-input displayOnlyInput" v-model.number="roundedArea" :class="{'input': true, 'is-valid': !errors.has('area') && this.currentArea != ''}" type="number" placeholder="0" name="area">
         </div>
-    
+
     </div>
 </template>
 
@@ -21,15 +20,24 @@ export default {
     props: ['roomsIndex'],
 
     computed: {
-        roundedArea: function () {
+        roundedArea: function() {
             if (this.$store.getters.rooms[this.roomsIndex].area === '' || this.$store.getters.rooms[this.roomsIndex].area === 0.00) return 0;
             return parseFloat(this.$store.getters.rooms[this.roomsIndex].area).toFixed(2);
         },
         currentArea() {
             return this.$store.getters.rooms[this.roomsIndex].area;
         }
+        // editingArea() {
+        //     return this.$store.getters.rooms[this.roomsIndex].area;
+        // }
     },
 
+    watch: {
+        currentArea() {
+            console.log('the current area changed.');
+            this.editingArea = this.$store.getters.rooms[this.roomsIndex].area;
+        }
+    },
     methods: {
         checkArea(area) {
             this.editingArea = validateInput(area, 0, 999, 0);
@@ -37,7 +45,7 @@ export default {
         }
     },
 
-    data: function () {
+    data: function() {
         return {
             editing: false,
             editingArea: this.$store.getters.rooms[this.roomsIndex].area
